082350ca293950ad69c32613a94320c9d429a777
xiaoqu <1483523635@qq.com>
2018-05-23 09:46:37 +0800

    Add project files.

---
+++ SSO.Passport/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SSO.Passport.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
